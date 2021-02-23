7093e7305d1b289e524328ddb3473cc3fe003816
safa <bjsafa30@gmail.com>
2021-02-23 17:30:25 +0100

    node

---
+++ baby-steps.js
@@ -0,0 +1,11 @@
+
+function sum(a , b, c) {
+
+    return (a+b+c)
+
+}
+
+let inputOne =Number(process.argv[2]);
+let inputTwo =Number(process.argv[3]);
+let inputthree =Number(process.argv[4]);
+ console.log(sum(inputOne,inputTwo,inputthree));
